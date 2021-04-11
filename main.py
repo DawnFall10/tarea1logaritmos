fa368258e7119af1c3282afdf235f077c2fab919
cinnamontea <sofia.castro@ing.uchile.cl>
2021-04-10 22:18:54 -0400

    optimization so it itsn't needed to open and close files every loop. Moved the iteration to exec_search

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import sys
 from bmh import boyer_moore_horspool
 from kmp import KMP
-from tools import exec_search
+from tools import exec_search, ROUND_BY
 
+'''
+Uso:
+python main.py <patrón> <ubicación del texto> <número de iteraciones>
+
+Si no se entrega el número adecuado de parámetros, se asumirá una búsqueda
+como la de los Experimentos 3 y 4 (Quijote, pero una sola vez). 
+
+Se retornan los resultados para ambos algoritmos.
+*Advertencia: ¡los loops son lentos, no usar números muy grandes!
+'''
+
+# Main checking
 if len(sys.argv) != 4:
-    pattern = "Quijote"
+    pattern = "Don Quijote de la Mancha"
     filename = "textfiles/quijoteEsp.txt"
     repeat = 1
 else:
@@ -12,27 +24,12 @@ else:
     filename = sys.argv[2]
     repeat = int(sys.argv[3])
 
+# Start experiment
 print("El patrón escogido fue \'" + pattern + "\'.")
 print("Se repetirá el experimento", repeat, "veces.")
 
-kmp_list = []
-bmh_list = []
-
-# KMP loop
-for i in range(0,repeat):
-    kmp_time = exec_search(filename, pattern, "KMP", KMP().search, i==repeat-1)
-    kmp_list.append(kmp_time)
-kmp_prom = sum(kmp_list)/len(kmp_list)
-print("Tiempo de ejecución promedio (segundos):\t", kmp_prom)
-print("Tiempo de ejecución total (segundos):\t\t", sum(kmp_list))
-
-# BMH loop
-for i in range(0,repeat):
-    bmh_time = exec_search(filename, pattern, "BMH", boyer_moore_horspool, i==repeat-1)
-    bmh_list.append(bmh_time)
-bmh_prom = sum(bmh_list)/len(bmh_list)
-print("Tiempo de ejecución promedio (segundos):\t", bmh_prom)
-print("Tiempo de ejecución total (segundos):\t\t", sum(bmh_list))
+kmp_prom = exec_search(filename, pattern, "KMP", KMP().search, repeat)
+bmh_prom = exec_search(filename, pattern, "BMH", boyer_moore_horspool, repeat)
 
 # Results
 if kmp_prom > bmh_prom:
@@ -41,5 +38,5 @@ else:
     best = "KMP"
 
 print("-- Resultado --")
-dif = kmp_prom - bmh_prom
-print(best, "fue el algoritmo más rápido por una diferencia promedio de", abs(dif), "segundos.")
+dif = abs(kmp_prom - bmh_prom)
+print(best, "fue el algoritmo más rápido por una diferencia promedio de", round(dif, ROUND_BY), "segundos.")
--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 import time
+ROUND_BY = 6
 
-def exec_search (filename, pattern, funcname, func, printBool):
+def exec_search (filename, pattern, funcname, func, repeat):
     try:
         # Validate arguments:
         if filename[-4:] != ".txt":
@@ -8,18 +9,26 @@ def exec_search (filename, pattern, funcname, func, printBool):
         elif pattern == "":
             raise SyntaxError("Error: el patrón ingresado debe contener al menos un caracter")
 
-        # Open file and execute function
+        # Open file and execute function:
         text = open(filename, encoding='utf-8')
-        tic1 = time.process_time()
-        result = func(text.read(), pattern)
-        tic2 = time.process_time()
+        alg_list = []
+        for i in range(0,repeat):
+            text.seek(0)
+            tic1 = time.process_time()
+            result = func(text.read(), pattern)
+            tic2 = time.process_time()
+            alg_time = tic2 - tic1
+            alg_list.append(alg_time)
         text.close()
 
-        delta = tic2 - tic1
-        if printBool:
-            print("--", funcname, "--")
-            print("Apariciones del patrón:\t\t\t\t", len(result))
-        return delta
+        # Printing data:
+        alg_prom = round(sum(alg_list)/len(alg_list), ROUND_BY)
+        total_time = round(sum(alg_list), ROUND_BY)
+        print("--", funcname, "--")
+        print("Apariciones del patrón:\t\t\t\t", len(result))
+        print("Tiempo de ejecución promedio (segundos):\t", alg_prom)
+        print("Tiempo de ejecución total (segundos):\t\t", total_time)
+        return alg_prom
 
     except:
         print(">:c")
